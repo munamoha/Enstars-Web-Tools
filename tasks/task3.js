4451c3ac9b614877019da6c4f2a54914dd0a96b7
munamoha <mm1982p@hotmail.com>
2023-10-31 05:36:21 -0400

    Fully implemented task3

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,22 +1,62 @@
-// JavaScript code for Task 3
-// Learning Objective 1: use of a sub-algorithm with one parameter
-// Learning Objective 2: use of a for loop
 
-function addSum(num) {
-  var p = document.getElementById("mydata");
-  var result = parseInt(mySum(num));
-  p.innerHTML = "Result = "+result+"";
+
+ function sum() {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var sums = parseInt(a) + parseInt(b);
+  var answer = "" + a + " + " + b + " = " + sums + ".";
+  document.getElementById("mydata").innerHTML = answer;
 }
 
-// a sub-algorithm to return sum of natural numbers up to num
-// pre-conditions: num belongs to Natural numbers >= 0
-// post-conditions: returns sum of all natural numbers less than or equal to num
-function mySum(num) {
-  let sum = 0;
- for (let i = 1; i <= num; i++) {
-   sum += i;
-  }
-  return sum;
-
+function difference()  {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var diff = parseInt(a) - parseInt(b);
+  var answer = "" + a + " - " + b + " = " + diff + ".";
+  document.getElementById("mydata").innerHTML = answer;
 }
+
+function product()  {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var prod = parseInt(a) * parseInt(b);
+  var answer = "" + a + " x " + b + " = " + prod + ".";
+  document.getElementById("mydata").innerHTML = answer;
+}
+
+
+function quotient()  {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var quo = parseInt(a) / parseInt(b);
+  var answer = "" + a + " ÷ " + b + " = " + quo + ".";
+  document.getElementById("mydata").innerHTML = answer;
+}
+
+function percentage() {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var per = (parseInt(a) / 100) * parseInt(b);
+  var answer = "" + a + "% of " + b + " = " + per + ".";
+  document.getElementById("mydata").innerHTML = answer;
+}
+
+function modulo() {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var per =parseInt(a) % parseInt(b);
+  var answer = "" + a + " mod " + b + " = " + per + ".";
+  document.getElementById("mydata").innerHTML = answer;
+}
+
+function power() {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var exp = Math.pow(parseInt(a), parseInt(b));
+  var answer = "" + a + "<sup>" + b + "</sup>" + " = " + exp + ".";
+  document.getElementById("mydata").innerHTML = answer;
+}
+
+
+
 
