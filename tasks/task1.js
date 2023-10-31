d21226ff5ca021022fef82969af89216447f25c5
munamoha <mm1982p@hotmail.com>
2023-10-31 03:10:07 -0400

    Finally got task2 working!!!

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,10 +1,4 @@
-// JavaScript code for Task 1
-// Learning Objective 1: use of sub-algorithm (aka function) with zero parameters
-// Learning Objective 2: use of Math.random()
 
-// a sub-algorithm to return a random number
-// pre-conditions: none
-// post-conditions: returns a random number in [0,2)
 function generateNum() {
   return Math.floor(Math.random()*2);
 }
@@ -20,14 +14,11 @@ function myFunction() {
     p.innerHTML = "TAILS";
   }
 
-  /*TODO 1: write an if statement here that--based on the value 
-   of num--changes the paragraph content to HEADS or TAILS */
-
 }
 
 
 
-  /*TODO 2: implement the body of the function here to return a random number between 0 and 2*/
+
 
 
 
