ec1ce94dd216e2b18a0441979868d978d345e30c
munamoha <mm1982p@hotmail.com>
2023-11-01 12:31:41 -0400

    Added rolling 2 d6 dice to task1

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,7 +2,7 @@
 function generateNum() {
   return Math.floor(Math.random()*2);
 }
-function myFunction() {
+function flip() {
   var p = document.getElementById("mydata");
 
   //call a sub-algorithm to return a random number in [0,2)
@@ -16,6 +16,24 @@ function myFunction() {
 
 }
 
+function rollDice() {
+  var p = document.getElementById("mydata");
+  var die1 = parseInt(rollDie());
+  var die2 = parseInt(rollDie());
+  if (die1 == die2) {
+    p.innerHTML = "Doubles! Dice rolls are "+die1+" and "+die2+"";
+  }
+  else {
+    p.innerHTML = "Dice rolls are "+die1+" and "+die2+"";
+  }
+
+}
+
+function rollDie() {
+  var num = Math.floor(Math.random() * 6);
+  num = num + 1;
+  return num;
+}
 
 
 
