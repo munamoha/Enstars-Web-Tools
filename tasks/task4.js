7b7907988aa44c99187bcaa7683e26395ad5495f
munamoha <mm1982p@hotmail.com>
2023-11-03 14:25:34 -0400

    starting on time tools

--- tasks/task4.js
+++ tasks/task4.js
@@ -1,36 +1,50 @@
-// JavaScript code for Task 5
-// Learning Objectives: use of arrays and use of object Date
+let date = new Date();
+let time;
+let p = document.getElementById("mydata");
+let allMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let allDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function myFunction() {
-  var p = document.getElementById("mydata");
-  //call a sub-algorithm to return the current time and date in a specific format
-  p.innerHTML = getDateInCustomizedFormat();
+
+function setDate() {
+  date = new Date(document.getElementById("calendar").value+"T00:00:00");
+  if (document.getElementById("calendar").value == "") {
+    p.innerHTML = "No input :( Please input a date.";
+  }
+  else {
+    p.innerHTML = "Your input: " + allDays[date.getDay()] + ", " + allMonths[date.getMonth()] + " " + date.getDate() + ", "  + date.getFullYear() + "!";
+  }
 }
 
-// a sub-algorithm to return the current date in a customized format
-// pre-conditions: none
-// post-conditions: returns current time and date in the following format: "It's HOUR:MIN. Today is DAYOFWEEK (MONTH DAY, YEAR)."
-//                  where capital words are replaced by appropriate values.
-function getDateInCustomizedFormat() {
-  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var d = new Date();
-  var dayOfWeek = d.getDay();
-  var month = d.getMonth();
-  var day = d.getDate();
-  var year = d.getFullYear();
-  var hour = d.getHours();
-  var ampm = "AM";
 
-  if (hour > 12) {
-    hour = hour -  12;
-    ampm = "PM";
+function setTime() {
+  time = document.getElementById("time").value;
+  if (time == "") {
+    p.innerHTML = "No input :( Please input a time.";
+  }
+  else {
+    p.innerHTML = time;
+  }
+}
+
+const isOverflown = ({ clientHeight, scrollHeight }) => scrollHeight > clientHeight;
+
+const resizeText = ({ element, parent }) => {
+  let i = 12 // let's start with 12px
+  let overflow = false
+  const maxSize = 128 // very huge text size
+
+  while (!overflow && i < maxSize) {
+    element.style.fontSize = `${i}px`
+    overflow = isOverflown(parent)
+    if (!overflow) i++
   }
 
-  var min = d.getMinutes();
-
-  /* TODO 1: complete the following statement such that the current time and date are shown with 
-     a format similar to that of the corresponding figure in Lab07 specification */ 
-  var result = "It's "+hour+":"+min+" "+ampm+". Today is "+days[dayOfWeek]+", "+months[month]+" "+day+", "+year+".";
-  return result;
+  // revert to last state where no overflow happened:
+  element.style.fontSize = `${i - 1}px`
 }
+
+
+resizeText({
+  element:  document.getElementById("mydata").innerHTML,
+  parent: document.getElementById("mydata")
+})
