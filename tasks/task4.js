4daf9f65d09281e2ff14281f67bd2034b04a8391
Muna Mohamed <mm1982p@hotmail.com>
2023-11-05 22:20:25 -0500

    added task to compare differences between dates

--- tasks/task4.js
+++ tasks/task4.js
@@ -29,6 +29,98 @@ function setTime() {
   }
 }
 
+function betweenTwo() {
+ const button = document.createElement('button');
+ const button2 = document.createElement('button');
+ button.id = 'setDate';
+ button2.id = 'set2Dates';
+ button.innerText = "Between Set Date and New Date";
+ button2.innerText = "Between Two New Dates";
+p.innerHTML = "Choose to either compare with the set date, or with two different dates.";
+document.getElementById('mydata').appendChild(button);
+document.getElementById('mydata').appendChild(button2);
+
+ button.addEventListener('click', () => {
+  p.innerHTML = "Set a new date to compare.\n";
+  const date2 = document.createElement('input');
+  date2.type = "date";
+  date2.id = "second";
+  document.getElementById('mydata').appendChild(date2);
+  const button3 = document.createElement('button');
+  button3.id = "select1";
+  button3.innerHTML = "Calculate Difference";
+  document.getElementById('mydata').appendChild(button3);
+
+  button3.addEventListener('click',  () => {
+    calculateDifference(date)
+  } )
+})
+
+button2.addEventListener('click', () => {
+  p.innerHTML = "Set two new dates to compare.\n";
+  const date2 = document.createElement('input');
+  date2.type = "date";
+  date2.id = "second";
+  document.getElementById('mydata').appendChild(date2);
+  const date3 = document.createElement('input');
+  date3.type = "date";
+  date3.id = "third";
+  document.getElementById('mydata').appendChild(date3);
+  const button3 = document.createElement('button');
+  button3.id = "select1";
+  button3.innerHTML = "Calculate Difference";
+  document.getElementById('mydata').appendChild(button3);
+
+  button3.addEventListener('click',  () => {
+    calculateDifference(date3.value)
+  } )
+}
+
+)
+
+
+}
+
+function calculateDifference(date1Input) {
+  let date2Input = document.getElementById("second").value;
+
+ // Parse the input dates into Date objects
+  let date1 = new Date(date1Input);
+  let date2 = new Date(date2Input);
+
+  // Calculate the time difference in milliseconds
+  let difference = Math.abs(date2 - date1);
+
+  // Convert milliseconds to days
+  let daysDifference = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  // Convert days
+  let yearsDifference = Math.floor(daysDifference / 365);
+  let monthsDifference = Math.floor((daysDifference % 365) / 30);
+  let remainingDays = Math.floor(daysDifference % 30);
+
+  //Display the result
+  let resultElement = document.getElementById("mydata");
+
+  let result = [];
+
+if (yearsDifference > 0) {
+    result.push(`${yearsDifference} year${yearsDifference !== 1 ? "s" : ""}`);
+  }
+  if (monthsDifference > 0) {
+    result.push(`${monthsDifference} month${monthsDifference !== 1 ? "s" : ""}`);
+  }
+  if (remainingDays > 0) {
+    result.push(`${remainingDays} day${remainingDays !== 1 ? "s" : ""}`);
+  }
+
+  if (result.length === 0) {
+    resultElement.textContent = "The dates are the same (or you didn't input them, you know who you are -_-).";
+  } else {
+    resultElement.textContent = `The difference between the two dates is ${result.join(", ")}.`;
+  }
+}
+
 const isOverflown = ({ clientHeight, scrollHeight }) => scrollHeight > clientHeight;
 
 const resizeText = ({ element, parent }) => {
